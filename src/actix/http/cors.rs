134ddadb05556240b3add3f8447dac6c7c8c379a
jbtrystram <jbtrystram@redhat.com>
2022-10-21 10:22:26 +0200

    add allowed methods and allow multiple URLs

--- src/actix/http/cors.rs
+++ src/actix/http/cors.rs
@@ -1,6 +1,14 @@
 use actix_cors::Cors;
 use serde::Deserialize;
 
+#[derive(Clone, Debug, Deserialize)]
+pub enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
+
 #[derive(Clone, Debug, Default, Deserialize)]
 pub struct CorsConfig {
     // default for bool is false
@@ -8,7 +16,10 @@ pub struct CorsConfig {
     pub allow_any_origin: bool,
 
     #[serde(default)]
-    pub allow_origin_url: Option<String>,
+    pub allow_origin_url: Option<Vec<String>>,
+
+    #[serde(default)]
+    pub allowed_methods: Option<Vec<Method>>
 }
 
 impl From<CorsConfig> for Cors {
@@ -23,8 +34,26 @@ impl From<CorsConfig> for Cors {
         if cfg.allow_any_origin {
             cors = cors.allow_any_origin();
         } else if let Some(origin) = &cfg.allow_origin_url {
-            cors = cors.allowed_origin(origin.as_str());
+            for url in origin {
+                cors = cors.allowed_origin(url.as_str());
+            }
+        }
+
+        if let Some(methods) = cfg.allowed_methods {
+            let methods: Vec<&str> = methods.into_iter().map(|m| m.into()).collect();
+            cors = cors.allowed_methods(methods);
         }
         cors
+    }
+}
+
+impl From<Method> for &str {
+    fn from(method: Method) -> Self {
+        match method {
+            Method::GET => "GET",
+            Method::POST => "POST",
+            Method::PUT => "PUT",
+            Method::DELETE => "DELETE",
+        }
     }
 }
