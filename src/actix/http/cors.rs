5f4e497b2ab173a2866d0e4c7d748e218939df6e
Jean-Baptiste Trystram <jbtrystram@redhat.com>
2022-10-24 10:59:24 +0200

    Use comma separated vec to allow multiple values

--- src/actix/http/cors.rs
+++ src/actix/http/cors.rs
@@ -1,3 +1,4 @@
+use crate::core::config::CommaSeparatedVec;
 use actix_cors::Cors;
 use http::Method;
 use serde::Deserialize;
@@ -10,22 +11,22 @@ pub struct CorsConfig {
     pub allow_any_origin: bool,
 
     #[serde(default)]
-    pub allow_origin_url: Option<Vec<String>>,
+    pub allow_origin_url: Option<CommaSeparatedVec>,
 
     #[serde(default)]
-    pub allowed_methods: Option<Vec<String>>,
+    pub allowed_methods: Option<CommaSeparatedVec>,
 }
 
 impl CorsConfig {
     pub fn set_allowed_methods(&mut self, methods: Vec<&str>) -> &Self {
         let methods: Vec<String> = methods.into_iter().map(|m| m.into()).collect();
-        self.allowed_methods = Some(methods);
+        self.allowed_methods = Some(methods.into());
         self
     }
 
     pub fn set_allowed_urls(&mut self, urls: Vec<&str>) -> &Self {
         let url: Vec<String> = urls.into_iter().map(|m| m.into()).collect();
-        self.allow_origin_url = Some(url);
+        self.allow_origin_url = Some(url.into());
         self
     }
 }
@@ -42,13 +43,14 @@ impl From<CorsConfig> for Cors {
         if cfg.allow_any_origin {
             cors = cors.allow_any_origin();
         } else if let Some(origin) = &cfg.allow_origin_url {
-            for url in origin {
+            for url in &origin.0 {
                 cors = cors.allowed_origin(url.as_str());
             }
         }
 
         if let Some(methods) = cfg.allowed_methods {
             let methods: Vec<Method> = methods
+                .0
                 .into_iter()
                 .filter_map(|m| Method::from_str(m.as_str()).ok())
                 .collect();
